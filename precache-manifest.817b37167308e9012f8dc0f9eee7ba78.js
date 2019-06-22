068f42d2726471a4345d2df9b9fae0f9aa9ac6d9
Santosh Kumar <kasantosh@yahoo.com>
2019-06-21 23:50:12 -0400

    Updates

---
+++ precache-manifest.817b37167308e9012f8dc0f9eee7ba78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "473fe0811bc03aaad64f032d975b2d99",
+    "url": "/clicky-game-react/index.html"
+  },
+  {
+    "revision": "12852c135b5960fdf725",
+    "url": "/clicky-game-react/static/css/main.9c76d2f8.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/clicky-game-react/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "12852c135b5960fdf725",
+    "url": "/clicky-game-react/static/js/main.aae0c216.chunk.js"
+  },
+  {
+    "revision": "96e9a9b550dae971e8fe",
+    "url": "/clicky-game-react/static/js/runtime~main.4ef103c0.js"
+  },
+  {
+    "revision": "56ece066c982b6955ee854942e3bae27",
+    "url": "/clicky-game-react/static/media/blue-butterfly.56ece066.jpg"
+  }
+]);
